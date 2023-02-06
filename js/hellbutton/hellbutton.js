4c216c388ccf116349bd44dd1ecc5583586117ca
Soldy <4786022+Soldy@users.noreply.github.com>
2023-02-06 19:12:36 +0000

    the inner part
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- js/hellbutton/hellbutton.js
+++ js/hellbutton/hellbutton.js
@@ -1,15 +1,18 @@
 function hellButtonClass(width_, height_){
     this.render = function(){
-        _left_line = _renderBorderLeft(); 
-        _right_line = _renderBorderRight(); 
+        _renderBorderLeft(); 
+        _renderBorderRight(); 
+        _renderInner(); 
         _svg.appendChild(_left_line);
         _svg.appendChild(_right_line);
+        _svg.appendChild(_inner);
         return _svg;
     };
     const _create = (tag)=>document.createElementNS('http://www.w3.org/2000/svg', tag);
     const _svg = _create('svg');
     let _right_line; 
     let _left_line; 
+    let _inner; 
     let _width = width_;
     let _height = height_;
     const _widthCalc = function(inp){return (_width/2000)*inp;};
@@ -29,26 +32,25 @@ function hellButtonClass(width_, height_){
                ' '+
                _renderPoint(point)
              );
-         console.log(out);
          return out ;
     };
     const _renderBorderStyle = function(el){
         el.setAttribute('fill', 'none');
-        el.setAttribute('stroke', 'blue');
+        el.setAttribute('stroke', '#ccc');
         el.setAttribute('stroke-width', '1');
 
     };
     const _renderBorderLine = function(direct){
         return _renderLine([
           [direct*200,200],
-          [0,400],
+          [0,600],
           [direct*-200,200]
         ]);
     };
-    const _renderBorderRight = function(){
+    const _renderBorder = function(points, direction){
         const out = _create('path');
-        let start = _renderPoint([2000-200,200]);
-        let line = _renderBorderLine(1);
+        let start = _renderPoint(points);
+        let line = _renderBorderLine(direction);
         out.setAttribute(
           'd',
           (
@@ -57,19 +59,27 @@ function hellButtonClass(width_, height_){
         );
         _renderBorderStyle(out);
         return out;
+
+    };
+    const _renderBorderRight = function(){
+        _right_line = _renderBorder(
+           [2000-200,0],
+           1
+        );
     };
     const _renderBorderLeft = function(){
-        const out = _create('path');
-        let start = _renderPoint([200,200]);
-        let line = _renderBorderLine(-1);
-        out.setAttribute(
-          'd',
-          (
-            ' M '+start+' l '+line
-          )
+        _left_line = _renderBorder(
+           [200,0],
+           -1
         );
-        _renderBorderStyle(out);
-        return out;
+    };
+    const _renderInner = function(){
+        _inner = _create('rect');
+       _inner.setAttribute('fill', '#ffff00');
+       _inner.setAttribute('x', _widthCalc(200));
+       _inner.setAttribute('y', _heightCalc(200));
+       _inner.setAttribute('width', _widthCalc(1600));
+       _inner.setAttribute('height', _heightCalc(600));
     };
     _svg.setAttribute('height', height_);
     _svg.setAttribute('width', width_);
