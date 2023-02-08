3d3074970cd44bc54a41a7597bb4873213d2e182
Soldy <4786022+Soldy@users.noreply.github.com>
2023-02-08 15:20:31 +0000

    image support
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- js/hellbutton/hellbutton.js
+++ js/hellbutton/hellbutton.js
@@ -1,11 +1,16 @@
-function hellButtonClass(width_, height_){
+function hellButtonClass(width_, height_, options_){
+    options_ = options_ || {};
     this.render = function(){
         _renderBorderLeft(); 
         _renderBorderRight(); 
-        _renderInner(); 
+           _renderInner(); 
+           _svg.appendChild(_inner);
+        _renderText();
+        _renderImage();
         _svg.appendChild(_left_line);
         _svg.appendChild(_right_line);
-        _svg.appendChild(_inner);
+        _svg.appendChild(_image);
+        _svg.appendChild(_text);
         _shell.appendChild(_svg);
         return _shell;
     };
@@ -16,9 +21,15 @@ function hellButtonClass(width_, height_){
 
     };
     const _svg = _create('svg');
+    let _text_color = options_.text_color || '#fff';
+    let _text_text = options_.text || '';
+    let _image_url = options_.image_url || '';
+    let _inner_color = options_.inner_color || '#0000';
     let _right_line; 
     let _left_line; 
     let _inner; 
+    let _image; 
+    let _text;
     let _width = width_;
     let _height = height_;
     let _shell = document.createElement('nav');
@@ -83,14 +94,38 @@ function hellButtonClass(width_, height_){
     const _renderInner = function(){
         _inner = _create('rect');
         _attr(_inner,{
-          'fill': '#ffff00',
-          'x': _widthCalc(200),
-          'y': _heightCalc(200),
-          'width': _widthCalc(1600),
-          'height': _heightCalc(600)
+          'fill': _inner_color,
+          x: _widthCalc(200),
+          y: _heightCalc(200),
+          width: _widthCalc(1600),
+          height: _heightCalc(600)
        });
     };
-    _attr(_svg, {height: height_,width: width_});
+    const _renderText = function(){
+        _text = _create('text');
+        _attr(_text,{
+          'text-anchor':'middle',
+          fill: _text_color,
+          'font-family':'Verdana,Geneva,DejaVu Sans,sans-serif',
+          'font-weight':'bold',
+          'font-size':_widthCalc(200),
+          x: _widthCalc(1000),
+          y: _heightCalc(550)
+        });
+        _text.textContent = _text_text;
+
+    };
+    const _renderImage = function(){
+        _image = _create('image');
+        _attr(_image,{
+          href: _image_url,
+          x: _widthCalc(200),
+          y: _heightCalc(200),
+          width: _widthCalc(1600),
+          height: _heightCalc(600)
+        });
+    };
+    _attr(_svg, {height: height_,width: width_,vievbox:'0 0 '+width_+' '+height_,xmlns:'http://www.w3.org/2000/svg'});
     _attr(_shell, {class:'hell_button_shell'});
 
 }
--- test/hellbutton/test.js
+++ test/hellbutton/test.js
@@ -1,10 +1,26 @@
 window.onload=function(){
-    const hellButton1 = new hellButtonClass(200,100);
-    const hellButton2 = new hellButtonClass(200,100);
+    const hellButton1 = new hellButtonClass(200,100, {
+      text:'TEST'
+    });
+    const hellButton2 = new hellButtonClass(200,100, {
+      text:'TEST',
+      text_color:'#000000',
+      image_url:'test.jpg'
+    });
+    const hellButton3 = new hellButtonClass(200,100, {
+      text:'TEST',
+      inner_color:'#0000ff'
+    });
     document.getElementsByTagName('body')[0].appendChild(
         hellButton1.render()
     );
     document.getElementsByTagName('body')[0].appendChild(
         hellButton2.render()
+    );
+    document.getElementsByTagName('body')[0].appendChild(
+        hellButton3.render()
+    );
+    document.getElementsByTagName('body')[0].appendChild(
+        (new hellButtonClass(200,100)).render()
     );
 };
